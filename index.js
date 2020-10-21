f02a2fee5e3670f663cc0c0c93bf50db640e737f
PajaCzechita <p.bajgarova@gmail.com>
2020-10-21 22:45:02 +0200

    adding if statement

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict'
 
-/*Vytvoř si proměnnou, ve které bude uloženo kdo je na tahu. Začíná vždy kolečko, tak rovnou do proměnné přiřaď hodnotu circle.*/
+/*KROK 1 Vytvoř si proměnnou, ve které bude uloženo kdo je na tahu. Začíná vždy kolečko, tak rovnou do proměnné přiřaď hodnotu circle.*/
+/* KRO.K 2 Při kliknutí do pole ořidám třídu na btn__game, kt vyzobrazí kolečko*/
+
+
+
+
+// pri kliku na tlacitko:
+// 1. if circle is playing, it draws circle into current field
+// 2. if cross is playing, it draws cross into current field
+// 3. if there is already circle/cross, it does nothing
+// 4. change current player
+// 5. compute if there is a winner
+
+// 1. 
+
 
 let addMove = 'circle';
 
-const getBtnElm = document.querySelectorAll('.btn__game').forEach(btn => {
-  btn.addEventListener("click", () => {
-    console.log('Ahoj')
+const btnElements = document.querySelectorAll('.btn__game');
+
+btnElements.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.currentTarget.classList.add('circle')
     });
 })
+
+
 
 
 
