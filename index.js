ed313e2cd6f88e89aa326717466829bc8635f4f2
PajaCzechita <p.bajgarova@gmail.com>
2020-10-21 22:07:13 +0200

    adding selection of btn Class

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict'
+
+/*Vytvoř si proměnnou, ve které bude uloženo kdo je na tahu. Začíná vždy kolečko, tak rovnou do proměnné přiřaď hodnotu circle.*/
+
+let addMove = 'circle';
+
+const getBtnElm = document.querySelectorAll('.btn__game').forEach(btn => {
+  btn.addEventListener("click", () => {
+    console.log('Ahoj')
+    });
+})
+
+
+
+
+
