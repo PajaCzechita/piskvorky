56f463ce3ee3225f08a0696a496b7662ac094b8b
PajaCzechita <p.bajgarova@gmail.com>
2020-10-29 23:03:45 +0100

    most of the function

--- index.js
+++ index.js
@@ -7,41 +7,44 @@
 // 4. change current player
 // 5. compute if there is a winner
 
-
+/* START - fce for adding class & see who is playin */
 let currentPlayer = 'circle';
 let playersSymbol = document.querySelector('.symbol').classList.add('circle');
 
 const btnElements = document.querySelectorAll('.btn__game');
 
 const onBtnClick = (e) => {
-  if (currentPlayer === 'circle') {
-      e.currentTarget.classList.add('circle__symbol');
-      e.currentTarget.disabled=true;
-      currentPlayer = 'cross';
+    if (currentPlayer === 'circle') {
+        e.currentTarget.classList.add('circle__symbol');
+        e.currentTarget.disabled = true;
+        currentPlayer = 'cross';
 
-      //adjust X/O -> HRAJE
-      playersSymbol = document.querySelector('.symbol').classList.remove('circle');
-      playersSymbol = document.querySelector('.symbol').classList.add('cross');
+        //adjust X/O -> HRAJE
+        playersSymbol = document.querySelector('.symbol').classList.remove('circle');
+        playersSymbol = document.querySelector('.symbol').classList.add('cross');
 
-     } else if ((currentPlayer === 'cross')) {
-      e.currentTarget.classList.add('cross__symbol');
-      e.currentTarget.disabled=true;
-      currentPlayer = 'circle';
+    } else if ((currentPlayer === 'cross')) {
+        e.currentTarget.classList.add('cross__symbol');
+        e.currentTarget.disabled = true;
+        currentPlayer = 'circle';
 
-      //adjust X/O -> HRAJE
-      playersSymbol = document.querySelector('.symbol').classList.remove('cross');
-      playersSymbol = document.querySelector('.symbol').classList.add('circle');
-     } 
-  }
+        //adjust X/O -> HRAJE
+        playersSymbol = document.querySelector('.symbol').classList.remove('cross');
+        playersSymbol = document.querySelector('.symbol').classList.add('circle');
+    }
+
+    isWinningMove(e.currentTarget);
+
+}
 
 btnElements.forEach(btn => {
-  btn.addEventListener('click', onBtnClick) 
+    btn.addEventListener('click', onBtnClick)
 });
+/* END of function */
 
-
-/* fnce for getting the field */
+/* START fnce for getting position of specific field */
 const gameField = 10; //size of the gameField
-const wholeField = document.querySelectorAll('game__field'); //select the whole game field
+const wholeField = document.querySelectorAll('.btn__game'); //select the whole game field
 
 /* 
 1/ create variable that starts with 0
@@ -53,14 +56,89 @@ const wholeField = document.querySelectorAll('game__field'); //select the whole 
 */
 
 const getPosition = (field) => {
-let fieldIndex = 0; 
-while (fieldIndex < wholeField.length) {
-  if (field === wholeField[fieldIndex]) {
-    break
-  } fieldIndex++
+    let fieldIndex = 0;
+    while (fieldIndex < wholeField.length) {
+        if (field === wholeField[fieldIndex]) {
+            break
+        } fieldIndex++
+    }
+    return {
+        row: Math.floor(fieldIndex / gameField),
+        column: fieldIndex % gameField
+    }
 }
-return {
-  row: Math.floor(fieldIndex/gameField),
-  column: fieldIndex % gameField
+
+//console.log(getPosition(wholeField[30]))
+
+/* END of function */
+
+const getField = (row, column) => {
+
+    const index = row * 10 + column; //gives index of the field
+    return wholeField[index] //gives the filed on the counted index
 }
+
+/* START fce, which return 'cross' or 'circle' or undefined. */
+
+const getSymbol = (field) => {
+
+    if (field.classList.contains('cross__symbol')) {
+        return 'cross'
+    } else if (field.classList.contains('circle__symbol')) {
+        return 'circle'
+    }
 }
+/* END of the function */
+
+/* START fnce that counts cross & circle and gives winner */
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+    const origin = getPosition(field)
+    const symbol = getSymbol(field)
+
+    let i
+    let inRow = 1 //number for current clicked field
+
+    //look to the left
+    i = origin.column
+    while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+        inRow++
+        i--
+    }
+
+    //look to the right
+    i = origin.column
+    while (i < boardSize - 1 && symbol === getSymbol(getField(origin.row, i + 1))) {
+        inRow++
+        i++
+    }
+
+    if (inRow >= symbolsToWin) {
+        return true
+    }
+
+
+    let inColumn = 1 //number for current clicked field
+
+    //look to the top
+    i = origin.column
+    while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+        inColumn++
+        i--
+    }
+
+    //look to the bottom
+    i = origin.column
+    while (i < boardSize - 1 && symbol === getSymbol(getField(i + 1, origin.column))) {
+        inColumn++
+        i++
+    }
+
+    if (inColumn >= symbolsToWin) {
+        return true
+    }
+
+    return false
+}
+
+/* END of the function */
